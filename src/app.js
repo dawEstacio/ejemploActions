bd2626c350bb45940c773a4cb1b578c27239a8b0
jovisal1 <pvidalsalvador@gmail.com>
2022-12-07 09:11:55 +0100

    Primera action creada

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+let express = require("express");
+let app = express();
+let port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
